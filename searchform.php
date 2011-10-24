601da8f10b9b952a97b32dba0b145421ea008604
Paul Bennett <p4blo32@gmail.com>
2011-10-24 22:00:52 +0100

    Searchform markup

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<input type="text" value="Search" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}" name="s" id="s" />
+	<input type="text" placeholder="Search" name="s" id="s" />
 </form>
