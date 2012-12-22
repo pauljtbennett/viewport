ccceb41611a7d66017c7a1d078fd9109f7be8758
Paul Bennett <p4blo32@gmail.com>
2012-12-22 19:00:13 +0000

    Fix missing background bug

--- config.rb
+++ config.rb
@@ -4,14 +4,14 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "image"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
