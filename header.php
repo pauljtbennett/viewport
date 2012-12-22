7ef1f57bfb5a2c519fb8841510f76c92f9fc8e3a
Paul Bennett <p4blo32@gmail.com>
2012-12-22 18:11:59 +0000

    Move scripts to footer

--- header.php
+++ header.php
@@ -8,15 +8,17 @@
 
 		<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,600italic,300,600" type="text/css" />
 		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/screen.css" type="text/css" media="screen" />
+
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 		<?php wp_deregister_script('jquery'); ?>
 
-		<?php wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', array(), null); ?>
-		<?php wp_enqueue_script('flexslider', get_bloginfo('template_directory') . '/js/flexslider/jquery.flexslider.js', array('jquery'), null); ?>
-		<?php wp_enqueue_script('functions',  get_bloginfo('template_directory') . '/js/functions.js', array('jquery'), null); ?>
+		<?php wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', 				array(), null, true); ?>
+		<?php wp_enqueue_script('flexslider', get_bloginfo('template_directory') . '/js/flexslider/jquery.flexslider.js',	array('jquery'), null, true); ?>
+		<?php wp_enqueue_script('functions',  get_bloginfo('template_directory') . '/js/functions.js', 						array('jquery'), null, true); ?>
 
 		<!--[if lt IE 9]>
 			<script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
